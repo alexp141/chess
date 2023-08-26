e24d821ef201a1bd5571da06dc210c691aebd245
Alexander Pastor <apastor@umd.edu>
2023-08-26 14:46:48 -0400

    bishop implementation

---
+++ src/engine/pieces/Bishop.java
@@ -0,0 +1,55 @@
+package engine.pieces;
+
+import engine.Team;
+import engine.board.*;
+import engine.util.Position;
+import engine.util.Utility;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bishop extends Piece {
+    public Bishop(Position position, Team team) {
+        super(position, team);
+    }
+
+    @Override
+    public List<Move> calculateMoves(Board board) {
+        List<Move> possibleMoves = new ArrayList<>();
+        int currX = this.position.getX();
+        int currY = this.position.getY();
+        //top-left
+        checkLine(board, possibleMoves, currX-1,currY-1,-1, -1);
+        //top-right
+        checkLine(board, possibleMoves, currX+1,currY-1,1, -1);
+        //bottom-left
+        checkLine(board, possibleMoves, currX-1,currY+1,-1, 1);
+        //bottom-right
+        checkLine(board, possibleMoves, currX+1,currY+1,1, 1);
+
+        return possibleMoves;
+    }
+
+    private void checkLine(Board board, List<Move> possibleMoves, int currX, int currY, int dx, int dy) {
+        if(!Utility.checkIfInBounds(currX,currY)) {
+            return;
+        }
+
+        calculateMoveType(board, currX, currY, possibleMoves);
+        checkLine(board, possibleMoves, currX + dx, currY + dy, dx , dy);
+
+    }
+
+    private void calculateMoveType(Board board, int currX, int currY, List<Move> possibleMoves) {
+        Cell destinationCell = board.getCellAt(currX, currY);
+        if (destinationCell.isOccupied()) {
+            Piece pieceAtDestination = destinationCell.getPiece();
+            if (pieceAtDestination.getTeam() != this.getTeam()) {
+                possibleMoves.add(new AttackMove(board, this, new Position(currX, currY), pieceAtDestination));
+            }
+        }
+        else {
+            possibleMoves.add(new PassiveMove(board, this, new Position(currX, currY)));
+        }
+    }
+}
---
+++ test/engine/pieces/BishopTest.java
@@ -0,0 +1,23 @@
+package engine.pieces;
+
+import engine.Team;
+import engine.board.Board;
+import engine.board.Move;
+import engine.util.Position;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class BishopTest {
+    @Test
+    public void testCalculateMoves() {
+        Board board = new Board();
+        Piece bishop = new Bishop(new Position(7,7), Team.WHITE);
+        List<Move> ret = bishop.calculateMoves(board);
+        assertTrue(ret.size() == 7);
+        bishop = new Bishop(new Position(3,3), Team.WHITE);
+        assertTrue(bishop.calculateMoves(board).size() == 13);
+    }
+}
